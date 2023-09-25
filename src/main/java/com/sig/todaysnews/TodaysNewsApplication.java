8fd7d687c6e9e2ebc82bbb5ea1776f3a6c285262
DoDoGaMaRu <101071839+DoDoGaMaRu@users.noreply.github.com>
2023-09-25 22:45:05 +0900

    init

---
+++ src/main/java/com/sig/todaysnews/TodaysNewsApplication.java
@@ -0,0 +1,13 @@
+package com.sig.todaysnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodaysNewsApplication.class, args);
+    }
+
+}
