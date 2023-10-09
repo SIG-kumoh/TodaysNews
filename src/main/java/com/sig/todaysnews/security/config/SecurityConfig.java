ff7b86cebdc498ad030f4eaf1fa65f6d170921e7
DoDoGaMaRu <101071839+DoDoGaMaRu@users.noreply.github.com>
2023-10-09 17:38:56 +0900

    [v0.01 :: JWT] 권한 검증 로직 구현 완료

--- src/main/java/com/sig/todaysnews/security/config/SecurityConfig.java
+++ src/main/java/com/sig/todaysnews/security/config/SecurityConfig.java
@@ -1,12 +1,9 @@
 package com.sig.todaysnews.security.config;
 
-import com.sig.todaysnews.security.handler.JwtAccessDeniedHandler;
-import com.sig.todaysnews.security.handler.JwtAuthenticationEntryPoint;
-import com.sig.todaysnews.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,13 +12,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.sig.todaysnews.redis.RedisService;
+import com.sig.todaysnews.security.handler.JwtAccessDeniedHandler;
+import com.sig.todaysnews.security.handler.JwtAuthenticationEntryPoint;
+import com.sig.todaysnews.security.TokenProvider;
+
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final RedisService redisService;
 
     // PasswordEncoder는 BCryptPasswordEncoder를 사용
     @Bean
@@ -32,7 +37,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((sessionManagement) -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -47,7 +51,7 @@ public class SecurityConfig {
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                         .accessDeniedHandler(jwtAccessDeniedHandler)
                 )
-                .apply(new JwtSecurityConfig(tokenProvider)); // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig class 적용
+                .apply(new JwtSecurityConfig(tokenProvider, redisService)); // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig class 적용
 
         return httpSecurity.build();
     }
