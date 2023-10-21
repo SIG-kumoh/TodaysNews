a005912ccade0f967ae17bc345b67914d6323cc5
ILOHAS <ilohas0021@naver.com>
2023-10-21 13:56:40 +0900

    [Service :: NewsService ing...]

--- src/main/java/com/sig/todaysnews/persistence/repository/ClusterRepository.java
+++ src/main/java/com/sig/todaysnews/persistence/repository/ClusterRepository.java
@@ -2,6 +2,12 @@ package com.sig.todaysnews.persistence.repository;
 
 import com.sig.todaysnews.persistence.entity.Cluster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Date;
+import java.util.List;
 
 public interface ClusterRepository extends JpaRepository<Cluster, Long> {
+    @Query("SELECT c FROM Cluster c WHERE c.sid = :sid AND c.date = :date")
+    List<Cluster> findClustersBySidAndDate(@Param("sid") Long sid, @Param("date") Date date);
 }
--- src/main/java/com/sig/todaysnews/sevice/NewsService.java
+++ src/main/java/com/sig/todaysnews/sevice/NewsService.java
@@ -1,4 +1,60 @@
 package com.sig.todaysnews.sevice;
 
+import com.sig.todaysnews.dto.ClusterDto;
+import com.sig.todaysnews.persistence.entity.Cluster;
+import com.sig.todaysnews.persistence.repository.ClusterRepository;
+import com.sig.todaysnews.persistence.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class NewsService {
+    private final ClusterRepository clusterRepository;
+    private final ArticleRepository articleRepository;
+
+    public List<ClusterDto> getProposal() {
+        return null;
+    }
+
+    public List<ClusterDto> getSection(Long sid, Date date) {
+        List<Cluster> clusters = clusterRepository.findClustersBySidAndDate(sid, date);
+        List<ClusterDto> clusterDtos = clusters.stream()
+                .map(cluster -> {
+                    ClusterDto clusterDto = ClusterDto.builder().build();
+                    // TODO 클러스터 세팅
+                    return clusterDto;
+                })
+                .collect(Collectors.toList());
+        return clusterDtos;
+    }
+
+    public List<ClusterDto> getHotClusters() {
+        return null;
+    }
+
+    public ClusterDto getCluster(Long cid) {
+        Cluster cluster = clusterRepository.findById(cid).orElse(null);
+        ClusterDto clusterDto = null;
+
+        if (cluster != null) {
+            clusterDto = ClusterDto.builder().build();
+            // TODO clusterDto 세팅
+        }
+
+        return clusterDto;
+    }
+
+    public void deleteCluster(Long cid) {
+        clusterRepository.deleteById(cid);
+    }
+
+    public void deleteArticle(Long aid) {
+        articleRepository.deleteById(aid);
+    }
+
 }
